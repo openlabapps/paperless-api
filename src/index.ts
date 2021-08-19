aaf808ed3c2f4eee10245899932129ee3f4c7b4e
Tom Sacher <tom.sacher@rnd.de>
2021-08-19 16:46:43 +0200

    fix: require https library conditional

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import type { AxiosInstance } from 'axios';
 import axios from 'axios';
-import * as fs from 'fs';
-import * as https from 'https';
 import type { Config } from './types/Config';
 import type { CorrespondentResponse } from './types/Correspondents';
 import type { DocumentTypeResponse } from './types/DocumentTypes';
@@ -14,10 +12,7 @@ export class Paperless {
 
   constructor(config: Config) {
     const baseURL = config.port ? `${config.host}:${config.port}` : config.host;
-    this.instance = axios.create({
-      httpsAgent: new https.Agent({
-        rejectUnauthorized: !config.ignoreSSL,
-      }),
+    const options: Record<string, any> = {
       baseURL: `${baseURL}/api`,
       headers: {
         Accept: 'application/json; version=2',
@@ -26,7 +21,17 @@ export class Paperless {
         username: config.username,
         password: config.password,
       },
-    });
+    };
+
+    if (config.ignoreSSL) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+      const https = require('https');
+      options.httpsAgent = new https.Agent({
+        rejectUnauthorized: !config.ignoreSSL,
+      });
+    }
+
+    this.instance = axios.create(options);
   }
 
   /**
